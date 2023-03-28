59ec78db02b932b7a2929034ec74b29e036b61e9
jacaballero1 <jkan87@hotmail.com>
2023-03-28 08:55:31 -0500

    Cambio de ruta Welcome por Login

---
+++ database/seeders/TableUserSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class TableUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::factory()->create([
+             'name' => 'Test User',
+             'username' => 'admin', 
+             'email' => 'admin@gmail.com',
+             'password' => bcrypt('123')
+        ]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
+
+
 
 Route::middleware([
     'auth:sanctum',
